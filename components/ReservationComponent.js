d1ef7e1964bd72e42b4abb3c865f47996e34ed8d
Jan Wilson <janlo.wilson@gmail.com>
2020-06-13 13:04:04 -0700

    Workshop 3

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, StyleSheet, Modal, Picker, Switch, Button } from 'react-native';
+import { Text, View, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
+import * as Animatable from 'react-native-animatable';
 
 class Reservation extends Component {
     constructor(props) {
@@ -9,8 +10,7 @@ class Reservation extends Component {
         this.state = {
             campers: 1,
             hikeIn: false,
-            date: '',
-            showModal: false
+            date: ''
         }
     }
 
@@ -18,27 +18,45 @@ class Reservation extends Component {
         title: 'Reserve Campsite'
     }
 
-    toggleModal() {
-        this.setState({ showModal: !this.state.showModal });
-    }
-
-    handleReservation() {
-        console.log(JSON.stringify(this.state));
-        this.toggleModal();
-    }
-
     resetForm() {
         this.setState({
             campers: 1,
             hikeIn: false,
-            date: '',
-            showModal: false
+            date: ''
         });
     }
 
     render() {
+
+        const handleReservation = () => {
+            console.log(JSON.stringify(this.state));
+            const message = `Number of Campers: ${this.state.campers}
+            \nHike-In? ${this.state.hikeIn}
+            \nDate: ${this.state.date}`;
+    
+            Alert.alert(
+                'Begin Search?',
+                message,
+                [
+                    {
+                        text: 'Cancel',
+                        onPress: () => { 
+                            console.log('Reservation Search Canceled');
+                            this.resetForm(); 
+                        },
+                        style: 'cancel'
+                    },
+                    {
+                        text: 'OK',
+                        onPress: () => { this.resetForm() }
+                    }
+                ],
+                { cancelable: false }
+            )
+        }
+
         return (
-            <ScrollView>
+            <Animatable.View animation='zoomIn' duration={2000} delay={1000}>
                 <View style={styles.formRow}>
                     <Text style={styles.formLabel}>Number of Campers</Text>
                     <Picker
@@ -89,36 +107,16 @@ class Reservation extends Component {
                 </View>
                 <View style={styles.formRow}>
                     <Button
-                        onPress={() => this.handleReservation()}
+                        onPress={handleReservation}
                         title='Search'
                         color='#5637DD'
                         accessibilityLabel='Tap me to search for available campsites to reserve'
                     />
                 </View>
-                <Modal
-                    animationType={'slide'}
-                    transparent={false}
-                    visible={this.state.showModal}
-                    onRequestClose={() => this.toggleModal()}>
-                    <View style={styles.modal}>
-                        <Text style={styles.modalTitle}>Search Campsite Reservations</Text>
-                        <Text style={styles.modalText}>Number of Campers: {this.state.campers}</Text>
-                        <Text style={styles.modalText}>Hike-In?: {this.state.hikeIn ? 'Yes' : 'No'}</Text>
-                        <Text style={styles.modalText}>Date: {this.state.date}</Text>
-                        <Button
-                            onPress={() => {
-                                this.toggleModal();
-                                this.resetForm();
-                            }}
-                            color='#5637DD'
-                            title='Close'
-                        />
-                    </View>
-                </Modal>
-            </ScrollView>
+            </Animatable.View>
         );
     }
-}
+};
 
 const styles = StyleSheet.create({
     formRow: {
