35cad568b76adfd0b5121bf111b8325329016ed6
Jan Wilson <janlo.wilson@gmail.com>
2020-06-16 09:08:14 -0700

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Text, View, StyleSheet, Picker, Switch, Button, Alert } from 'react-native';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Permissions from 'expo-permissions';
+import { Notifications } from 'expo';
 
 class Reservation extends Component {
     constructor(props) {
@@ -26,6 +28,28 @@ class Reservation extends Component {
         });
     }
 
+    async obtainNotificationPermission() {
+        const permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            const permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+            return permission;
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        const permission = await this.obtainNotificationPermission();
+        if (permission.status === 'granted') {
+            Notifications.presentLocalNotificationAsync({
+                title: 'Your Campsite Reservation Search',
+                body: 'Search for ' + date + ' requested'
+            });
+        }
+    }
+
     render() {
 
         const handleReservation = () => {
@@ -33,22 +57,25 @@ class Reservation extends Component {
             const message = `Number of Campers: ${this.state.campers}
             \nHike-In? ${this.state.hikeIn}
             \nDate: ${this.state.date}`;
-    
+
             Alert.alert(
                 'Begin Search?',
                 message,
                 [
                     {
                         text: 'Cancel',
-                        onPress: () => { 
+                        onPress: () => {
                             console.log('Reservation Search Canceled');
-                            this.resetForm(); 
+                            this.resetForm();
                         },
                         style: 'cancel'
                     },
                     {
                         text: 'OK',
-                        onPress: () => { this.resetForm() }
+                        onPress: () => {
+                            this.presentLocalNotification(this.state.date);
+                            this.resetForm();
+                        }
                     }
                 ],
                 { cancelable: false }
